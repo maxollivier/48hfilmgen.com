a68c94707d5ea18034e4fb4b5740e47a3d0a3d9b
maxollivier <42263307+maxollivier@users.noreply.github.com>
2018-08-17 11:04:12 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,16 @@ function genreGenerator(){
 	var genreIndex = Math.floor(Math.random() * (genre.length));
 	var genrePrint = genre[genreIndex];
 	document.getElementById("genre").innerHTML = genrePrint;
+	console.log genreIndex
+	console.log genrePrint
 }
 
 function phraseGenerator(){
 	var phraseIndex = Math.floor(Math.random() * (phrase.length));
 	var phrasePrint = genre[phraseIndex];
 	document.getElementById("phrase").innerHTML = phrasePrint;
+	console.log phraseIndex
+	console.log phrasePrint
 }
 
 function generator(){
