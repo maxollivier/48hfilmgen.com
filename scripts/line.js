c268ef1127c644a42d249ad5b84acc6c2762770d
maxollivier <42263307+maxollivier@users.noreply.github.com>
2018-08-16 22:46:10 -0700

    Update line.js

--- scripts/line.js
+++ scripts/line.js
@@ -1,5 +1,9 @@
 var genre = [
-"One",
-"Two",
-"Three",
+'Who\'s line is it anyway?',
+'I\'ve got a bad feeling about this',
+'No, I am your father',
+'I can see you',
+'It wasn\'t me',
+'Wait! There\'s more',
+'I heard you'
 ]
