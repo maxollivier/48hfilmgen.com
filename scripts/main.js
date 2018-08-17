dac35e1fe284ecd7812ec6260ecc766d2bfad906
maxollivier <42263307+maxollivier@users.noreply.github.com>
2018-08-16 23:01:57 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,5 @@
 function generator(){
 		var genx = Math.floor(Math.random() * (genre.length));
-		var phrx = Math.floor(Math.random() * (phrase.length));
 		var geny = genre[genx];
-		var phry = phrase[phrx];
 		document.getElementById("genre").innerHTML = geny
-		document.getElementById("line").innerHTML = phry
 }
