51a4c0e76efcbe7f9d85bd6741fa87da7ad24267
maxollivier <42263307+maxollivier@users.noreply.github.com>
2018-08-16 23:00:22 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ function generator(){
 		var genx = Math.floor(Math.random() * (genre.length));
 		var phrx = Math.floor(Math.random() * (phrase.length));
 		var geny = genre[genx];
-		var phry = phrase[linx];
-		document.getElementById("genre").innerHTML = genx
-		document.getElementById("line").innerHTML = phrx
+		var phry = phrase[phrx];
+		document.getElementById("genre").innerHTML = geny
+		document.getElementById("line").innerHTML = phry
 }
